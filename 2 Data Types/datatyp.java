fbacb9bdd4b0dad5bbcaefb139cae83ecee9e9a4
tUrInG1uRKSS <marco.llontop.h@uni.pe>
2024-01-16 10:32:23 -0500

    Data Types in Java

---
+++ 2 Data Types/datatyp.java
@@ -0,0 +1,17 @@
+class datatyp
+{
+    public static void main(String a[])
+    {
+        byte by = 127;
+        short sh = 558;
+        int num1 = 9;
+        long l = 5854l;
+
+        float f = 5.8f;
+        double d = 5.8;
+
+        char c = 'k';
+
+        boolean b = true;
+    }
+}
