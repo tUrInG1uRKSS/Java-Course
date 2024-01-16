e059ed8f413df6cd27e15ccad88de449f97142c4
tUrInG1uRKSS <marco.llontop.h@uni.pe>
2024-01-16 11:02:24 -0500

    Literals

---
+++ 3 Literal/literal.java
@@ -0,0 +1,17 @@
+class literal
+{
+    public static void main(String a[])
+    {   
+        // literals
+        
+        // int num1 = 0b101;
+        // int num1 = 0x7E;
+        // int num1 = 10_000_000;
+        // double num1 = 13;
+        // double num1 = 12e10;
+
+        char c = 'a';
+        c++; // char treated as integer
+        System.out.println(c); 
+    }
+}
