a164e3eeb04ddd60e01b2dc6682a29fade77e547
tUrInG1uRKSS <marco.llontop.h@uni.pe>
2024-01-16 13:33:26 -0500

    Relational Operators

---
+++ 6 RelationalOp/Demo.java
@@ -0,0 +1,16 @@
+class Demo
+{
+    public static void main(String[] args) {   
+        
+        // int x = 6;
+        // int y = 6;
+        double x = 8.8;
+        double y = 9.8;
+
+        // boolean result = x != y;
+        // boolean result = x == y;
+        boolean result = x <= y;
+        
+        System.out.println(result);
+    }
+}
