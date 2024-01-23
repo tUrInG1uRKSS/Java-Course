b066c208e548e4c055dc4a7c4268d914efcc6703
tUrInG1uRKSS <marco.llontop.h@uni.pe>
2024-01-23 14:58:05 -0500

    Single, Multilevel and Multiple Inheritance

--- 24 Inheritance/Demo17.java
+++ 24 Inheritance/Demo17.java
@@ -1,14 +1,17 @@
+// Multiple inheritance don't work in Java
+
 public class Demo17 
 {
     public static void main(String a[])
     {
-        AdvCalc obj = new AdvCalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
         int r1 = obj.add(4, 5);
         int r2 = obj.sub(7, 3);
-        int r3 = obj.sub(5, 3);
-        int r4 = obj.sub(15, 3);
+        int r3 = obj.multi(5, 3);
+        int r4 = obj.div(15, 3);
+        double r5 = obj.power(4, 2);
 
-        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+        System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
     }
     
 }
