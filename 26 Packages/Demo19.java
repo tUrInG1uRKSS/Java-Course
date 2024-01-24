e0fffa4bb9a47928e3fd0466ec3bdf297e02ad4e
tUrInG1uRKSS <marco.llontop.h@uni.pe>
2024-01-24 13:56:46 -0500

    Structured Projects with Packages

---
+++ 26 Packages/Demo19.java
@@ -0,0 +1,20 @@
+// import com.google.calculation; // common naming
+
+// import java.util.ArrayList;
+
+// import tools.AdvCalc2;
+// import tools.Calc2;
+import other.tools.*;
+import other.*;
+
+// import java.lang.*; // by default
+
+public class Demo19 
+{
+    // ArrayList list = new ArrayList();
+
+    Calc2 obj = new Calc2();
+    AdvCalc2 obj1 = new AdvCalc2();
+
+    A obj2 = new A();
+}
