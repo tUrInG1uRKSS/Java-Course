b07f44307b6ddf37c600ced54412f0151bc233a4
tUrInG1uRKSS <marco.llontop.h@uni.pe>
2024-01-25 14:23:58 -0500

    Anonymous Inner class

--- 32 InnerClass/Demo32.java
+++ 32 InnerClass/Demo32.java
@@ -1,34 +1,46 @@
-class A 
+// class A 
+// {
+//     int age;
+
+//     public void show()
+//     {
+//         System.out.println("in A show");
+//     }
+
+//     static class B 
+//     {
+//         public void config()
+//         {
+//             System.out.println("in config");
+//         }
+//     }
+// }
+
+abstract class A 
 {
-    int age;
-
-    public void show()
-    {
-        System.out.println("in A show");
-    }
-
-    static class B 
-    {
-        public void config()
-        {
-            System.out.println("in config");
-        }
-    }
+    public abstract void show();
+    public abstract void config();
 }
 
 public class Demo32 
 {
     public static void main(String a[])
     {
+        // Anonymous Inner Class
         A obj = new A()
         {
             public void show()
             {
                 System.out.println("in new show");
             }
+            public void config()
+            {
+                System.out.println("in config");
+            }
         };
         obj.show();
 
+        // Inner Class
         // if B is non-static
         // A.B obj1 = obj.new B();
         // obj1.config();
